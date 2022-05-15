1698b6c03f9c7864db41087f883c6dd2705a5a0c
GuessingMachine <tjd125gns@naver.com>
2022-05-15 12:21:39 +0900

    로그인 뷰(view) 최적화 | MVC의 V(view) 분리하기 - npm으로 ejs 설치 - 뷰를 관리하는 파일(views) 불러옴 -  views파일에 있는 html을 어떤 엔진으로 해석할지 지정 -  views폴더안에 있는 ejs파일로 이동

--- login-lecture/app.js
+++ login-lecture/app.js
@@ -1,41 +1,18 @@
-const express = require("express") // require라는 명령어로 express 다운 받음
-const app = express(); //app라는 변수에 express 실행해서 넣어줌
+const express = require("express")
+const app = express();
 
-app.get("/",(req, res)=>{//서버의 루트 경로 지정
-    res.send(`<!DOCTYPE html>
-    <html lang="ko">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        여기는 루트입니다.
-    </body>
-    </html>`);
+// 앱 세팅
+app.set("views", "./views"); // 뷰를 관리하는 파일(views) 불러옴
+app.set("view engine", "ejs"); // views파일에 있는 html을 어떤 엔진으로 해석할지 지정
+
+app.get("/",(req, res)=>{
+    res.render("home/index");// views폴더안에 있는 ejs파일로 이동
 });
 
-app.get("/login",(req, res)=>{//로그인 화면 지정
-    res.send(`
-    <!DOCTYPE html>
-    <html lang="ko">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <input type="text" placeholder="아이디"><br>
-        <input type="text" placeholder="비밀번호"><br>
-        <button>로그인</button>
-    </body>
-    </html>
-    `
-    );
+app.get("/login",(req, res)=>{
+    res.render("home/login");
 });
 
-app.listen(3000, function() { // 서버 띄움 3000포트 열어달라
+app.listen(3000, function() {
     console.log("서버 가동");
 });
