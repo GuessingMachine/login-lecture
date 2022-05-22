7a74c4c9807d7d48460899e5dc6210e10ad0ddd7
GuessingMachine <tjd125gns@naver.com>
2022-05-22 22:51:45 +0900

    DOM으로 HTML 객체 제어하기
    - document.querySelector 으로 태그의 정보 가져옴
    - 선택자를 지정해서 해당하는 것을 가져옴 #을 넣어서 태그의 id로 부여된 것을 가져옴
    - addEventListener("click", login); //이벤트 버튼이 눌리게 되면, 함수가 작동

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,16 @@
 // front end
 "use strict"
 
-console.log("hello");
+const id = document.querySelector("#id"), //#은 태그의 id로 부여된 것을 가져온다
+ psword = document.querySelector("psw"),
+ loginBtn = document.querySelector("botton");
+
+loginBtn.addEventListener("click", login); //이벤트 버튼이 눌리게 되면, 함수가 작동
+
+function login(){
+    const req = {
+        id : id.value,
+        psw : psword.value
+    }
+}
+
