6b427b606ab1b1ffbfcc323213d475075221d63d
GuessingMachine <tjd125gns@naver.com>
2022-05-22 23:05:17 +0900

    로그인을 요청하는 fetch를 생성
    fetch("/login", {
            method: "POST", // body로 정보를 전달할 땐 POST방식 사용
            headers: { // 내가 보내는 데이터가 JSON이라고 알려줌
                "Content-Type": "application/json"
            },
            body: JSON.stringify(req) // JOSN 형태로 변경, stringify 문자열로 변경
        }); // 서버로 보냄 (경로, 보낼 내용) object 형태로 보내야함

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,16 +1,23 @@
 // front end
 "use strict"
 
-const id = document.querySelector("#id"), //#은 태그의 id로 부여된 것을 가져온다
+const id = document.querySelector("#id"), 
  psword = document.querySelector("psw"),
  loginBtn = document.querySelector("botton");
 
-loginBtn.addEventListener("click", login); //이벤트 버튼이 눌리게 되면, 함수가 작동
-
+loginBtn.addEventListener("click", login); 
 function login(){
     const req = {
         id : id.value,
         psw : psword.value
     }
+
+    fetch("/login", {
+        method: "POST", // body로 정보를 전달할 땐 POST방식 사용
+        headers: { // 내가 보내는 데이터가 JSON이라고 알려줌
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req) // JOSN 형태로 변경, stringify 문자열로 변경 
+    }); // 서버로 보냄 (경로, 보낼 내용) object 형태로 보내야함
 }
 
